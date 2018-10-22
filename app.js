864472ae8f0afab32b86db68fada7652851f9739
Anatolii <anatolii.lukavenko@gmail.com>
2018-10-22 10:23:10 +0000

    Added mongoose, connected to DB, made find() and  create () methods

--- app.js
+++ app.js
@@ -1,21 +1,45 @@
-var express = require("express");
-var app = express();
-var bodyParser =require("body-parser")
+var express         = require("express"),
+    app             = express(),
+    bodyParser      = require("body-parser"),
+    mongoose        = require("mongoose")
 
+mongoose.connect("mongodb://localhost:27017/enjoy_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");  
 
-var campgrounds = [
-            {name: "Village Colm", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f9c97fa0eebdba_340.jpg"},
-            {name: "Desert", image: "https://farm9.staticflickr.com/8131/8699858843_72fcfec2ca.jpg"},
-            {name: "Mountains", image: "https://farm3.staticflickr.com/2928/33123480065_630e89df61.jpg"},
-            {name: "Village Colm", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f9c97fa0eebdba_340.jpg"},
-            {name: "Desert", image: "https://farm9.staticflickr.com/8131/8699858843_72fcfec2ca.jpg"},
-            {name: "Mountains", image: "https://farm3.staticflickr.com/2928/33123480065_630e89df61.jpg"},
-            {name: "Village Colm", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f9c97fa0eebdba_340.jpg"},
-            {name: "Desert", image: "https://farm9.staticflickr.com/8131/8699858843_72fcfec2ca.jpg"},
-            {name: "Mountains", image: "https://farm3.staticflickr.com/2928/33123480065_630e89df61.jpg"},
-        ];
+// SCHEMA SETUP
+var campgroundSchema =  new mongoose.Schema({
+    name: String,
+    image: String
+});
+
+var Campground = mongoose.model("Campground", campgroundSchema);
+
+ //manual creating data in DB
+// Campground.create({
+//     name: "Village Colm",
+//     image: "https://pixabay.com/get/e83db40e28fd033ed1584d05fb1d4e97e07ee3d21cac104491f0c070a1e8b2bb_340.jpg"
+// }, function (err, campground) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log("newly created campground");
+//         console.log(campground);
+//     }
+// });
+
+
+// var campgrounds = [
+//             {name: "Village Colm", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f9c97fa0eebdba_340.jpg"},
+//             {name: "Desert", image: "https://farm9.staticflickr.com/8131/8699858843_72fcfec2ca.jpg"},
+//             {name: "Mountains", image: "https://farm3.staticflickr.com/2928/33123480065_630e89df61.jpg"},
+//             {name: "Village Colm", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f9c97fa0eebdba_340.jpg"},
+//             {name: "Desert", image: "https://farm9.staticflickr.com/8131/8699858843_72fcfec2ca.jpg"},
+//             {name: "Mountains", image: "https://farm3.staticflickr.com/2928/33123480065_630e89df61.jpg"},
+//             {name: "Village Colm", image: "https://pixabay.com/get/e83db50929f0033ed1584d05fb1d4e97e07ee3d21cac104496f9c97fa0eebdba_340.jpg"},
+//             {name: "Desert", image: "https://farm9.staticflickr.com/8131/8699858843_72fcfec2ca.jpg"},
+//             {name: "Mountains", image: "https://farm3.staticflickr.com/2928/33123480065_630e89df61.jpg"},
+//         ];
 
 
 app.get("/", function(req, res) {
@@ -23,8 +47,15 @@ app.get("/", function(req, res) {
 });
 
 app.get("/campgrounds", function (req, res) {
-    
-    res.render("campgrounds", {campgrounds: campgrounds});
+    //get all campgrounds from DB
+    Campground.find({}, function (err, allCampgrounds) {
+        if(err) {
+            console.log(err);
+        } else {
+            res.render("campgrounds", {campgrounds: allCampgrounds});
+        }
+    });
+    // res.render("campgrounds", {campgrounds: campgrounds});
         
 });
 
@@ -33,11 +64,21 @@ app.post("/campgrounds", function (req, res) {
     var name = req.body.name;
     var image = req.body.image;
     var newCampground = {name: name, image: image};
-    campgrounds.push(newCampground);
-    // redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    //create a new campground and save to DB
+    Campground.create(newCampground, function (err, newlyCreated) {
+        if(err) {
+            console.log(err);
+        } else {
+            // redirect back to campgrounds page
+            res.redirect("/campgrounds");
+        }
+    });
+    // campgrounds.push(newCampground);
     
-})
+    // redirect back to campgrounds page
+    // res.redirect("/campgrounds");
+    
+});
 
 app.get("/campgrounds/new",function(req, res) {
     res.render("new");
