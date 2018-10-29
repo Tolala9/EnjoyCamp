c252916f9f1d5b89b9a547c731f913d951bab67a
Anatolii <anatolii.lukavenko@gmail.com>
2018-10-29 10:10:47 +0000

    Associations: post display author name & requiring login for posting

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,7 +4,7 @@ var Campground          = require("../models/campground");
 
 //change app. to router.
 
-// INDEX - show all campgrounds
+//show all campgrounds
 router.get("/", function (req, res) {
     
     //get all campgrounds from DB
@@ -20,18 +20,23 @@ router.get("/", function (req, res) {
 });
 
 // CREATE - Add new campground to DB
-router.post("/", function (req, res) {
+router.post("/", isLoggedIn, function (req, res) {
     // get data form form and add to campgrounds array
     var name = req.body.name;
     var image = req.body.image;
     var description = req.body.description;
-    var newCampground = {name: name, image: image, description: description};
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newCampground = {name: name, image: image, description: description, author: author};
     //create a new campground and save to DB
     Campground.create(newCampground, function (err, newlyCreated) {
         if(err) {
             console.log(err);
         } else {
             // redirect back to campgrounds page
+            console.log(newlyCreated);
             res.redirect("/campgrounds");
         }
     });
@@ -41,7 +46,7 @@ router.post("/", function (req, res) {
 
 
 // NEW - show form to create new campground
-router.get("/new",function(req, res) {
+router.get("/new", isLoggedIn, function(req, res) {
     res.render("campgrounds/new");
 });
 
@@ -59,5 +64,13 @@ router.get("/:id", function(req, res) {
     });
     
 });
+
+// Middleware
+function isLoggedIn(req, res, next) {
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
 
 module.exports = router;
