3179c7c89bc001fb6fdfa3b33ba9b4565c50107e
Anatolii <anatolii.lukavenko@gmail.com>
2018-11-16 08:31:40 +0000

    add mongoLab

--- app.js
+++ app.js
@@ -17,7 +17,9 @@ var campgroundRoutes        = require("./routes/campgrounds"),
     commentRoutes           = require("./routes/comments"),
     indexRoutes             = require("./routes/index");
     
-mongoose.connect("mongodb://localhost:27017/enjoy_camp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost:27017/enjoy_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://tolala:tolala333@ds041377.mlab.com:41377/enjoy_camp", { useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");  
 app.use(express.static(__dirname + "/public"));
